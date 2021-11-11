c92bcd28795780b0a9b7f86c93245ef759e8cdb1
JaaronAlvian <94091982+JaaronAlvian@users.noreply.github.com>
2021-11-11 10:57:44 +0800

    Add files via upload

---
+++ db.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<?php 
+define('DB_USER', "root"); 
+define('DB_PASSWORD', ""); 
+define('DB_DATABASE', "pessdb"); 
+define('DB_SERVER', "localhost"); 
+?>
+<body>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<img src="Banner.jpg" style="max-width:100%" alt="Sample picture">
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<h1>Police Emergency Service System</h1>
+<nav>
+ <a href="logcall.php">Log Call</a>
+ <a href="Update.php">Update</a>
+ <a href="#">Report</a>
+ <a href="#">History</a>
+ </nav>
+<body>
+</body>
+</html>
