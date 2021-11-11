bb04db519b73d884e8b1eda5c21e276f88934971
JaaronAlvian <94091982+JaaronAlvian@users.noreply.github.com>
2021-11-11 11:03:04 +0800

    Add files via upload

---
+++ pessdb.sql
@@ -0,0 +1,196 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Nov 11, 2021 at 04:01 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pessdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dispatch`
+--
+
+CREATE TABLE `dispatch` (
+  `IncidentId` int(11) NOT NULL,
+  `patrolcarId` varchar(10) NOT NULL,
+  `timeDispatched` datetime NOT NULL,
+  `timeArrived` datetime DEFAULT NULL,
+  `timeCompleted` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `incident`
+--
+
+CREATE TABLE `incident` (
+  `IncidentId` int(11) NOT NULL,
+  `callerName` varchar(30) NOT NULL,
+  `phoneNumber` varchar(10) NOT NULL,
+  `incidentTypeId` varchar(3) NOT NULL,
+  `incidentLocation` varchar(50) NOT NULL,
+  `incidentDesc` varchar(100) NOT NULL,
+  `incidentStatusId` varchar(1) NOT NULL,
+  `timeCalled` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `incidenttype`
+--
+
+CREATE TABLE `incidenttype` (
+  `incidentTypeId` varchar(3) NOT NULL,
+  `incidentTypeDesc` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `incidenttype`
+--
+
+INSERT INTO `incidenttype` (`incidentTypeId`, `incidentTypeDesc`) VALUES
+('040', 'Domestic Violent'),
+('030', 'Burglary'),
+('020', 'Riot'),
+('010', 'Fire'),
+('070', 'Loan Shark'),
+('050', 'Fallen Tree'),
+('060', 'Traffic Accident'),
+('999', 'Others');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `incident_status`
+--
+
+CREATE TABLE `incident_status` (
+  `statusId` varchar(1) NOT NULL,
+  `statusDesc` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `incident_status`
+--
+
+INSERT INTO `incident_status` (`statusId`, `statusDesc`) VALUES
+('1', 'Pending'),
+('2', 'Dispatched'),
+('3', 'Complete'),
+('4', 'Duplicate');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patrolcar`
+--
+
+CREATE TABLE `patrolcar` (
+  `patrolcarId` varchar(10) NOT NULL,
+  `patrolcarStatusId` varchar(1) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `patrolcar`
+--
+
+INSERT INTO `patrolcar` (`patrolcarId`, `patrolcarStatusId`) VALUES
+('QX9876A', '3'),
+('QX5432B', '2'),
+('QX1234J', '4'),
+('QX1212E', '2'),
+('QX8576G', '3'),
+('QX0912N', '1'),
+('QX1256A', '2'),
+('QX8132H', '4'),
+('QX4598J', '4'),
+('QX9173U', '2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patrolcar_status`
+--
+
+CREATE TABLE `patrolcar_status` (
+  `statusId` varchar(1) NOT NULL,
+  `statusDesc` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `patrolcar_status`
+--
+
+INSERT INTO `patrolcar_status` (`statusId`, `statusDesc`) VALUES
+('1', 'Dispatched'),
+('2', 'Patrol'),
+('3', 'Free'),
+('4', 'Arrived');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `incident`
+--
+ALTER TABLE `incident`
+  ADD PRIMARY KEY (`IncidentId`);
+
+--
+-- Indexes for table `incidenttype`
+--
+ALTER TABLE `incidenttype`
+  ADD PRIMARY KEY (`incidentTypeId`);
+
+--
+-- Indexes for table `incident_status`
+--
+ALTER TABLE `incident_status`
+  ADD PRIMARY KEY (`statusId`);
+
+--
+-- Indexes for table `patrolcar`
+--
+ALTER TABLE `patrolcar`
+  ADD PRIMARY KEY (`patrolcarId`);
+
+--
+-- Indexes for table `patrolcar_status`
+--
+ALTER TABLE `patrolcar_status`
+  ADD PRIMARY KEY (`statusId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `incident`
+--
+ALTER TABLE `incident`
+  MODIFY `IncidentId` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
